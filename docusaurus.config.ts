9fb8adb3a593e8bd9787644f65464ee5c4762377
Лебедева Александра <a.lebedeva@iva.ru>
2026-07-15 17:36:39 +0300

    added the Antora series

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -12,12 +12,12 @@ const config: Config = {
   url: 'https://sandralbdvtw.github.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/docusaurus/',
+  baseUrl: '/portfolio/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
   organizationName: 'sandraLbdvTW', // Usually your GitHub org/user name.
-  projectName: 'docusaurus', // Usually your repo name.
+  projectName: 'portfolio', // Usually your repo name.
 
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
